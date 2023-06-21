69c0cdc38f895063210ff92540ecd41760d2055e
DevpeAr <admin@devpeg.ar>
2023-06-21 20:28:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const hbs = require("hbs");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
+// Handlebars
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+// Servir contenido estatico
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "Pablo Grilletti",
+    titulo: "Curso de node",
+  });
+});
+app.get("/generic", (req, res) => {
+  res.render("generic", {
+    nombre: "Pablo Grilletti",
+    titulo: "Curso de node",
+  });
+});
+app.get("/elements", (req, res) => {
+  res.render("elements", {
+    nombre: "Pablo Grilletti",
+    titulo: "Curso de node",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
